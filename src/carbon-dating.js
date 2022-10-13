65bb4ae54a84396f6b0766545b6f4c391a72013c
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2022-10-13 16:41:38 +0300

    feat: carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,22 +6,35 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') {
+    return false;
+  }
+  if (isNaN(+sampleActivity)) {
+    return false;
+  } else {
+    let rez = Math.ceil(
+      (HALF_LIFE_PERIOD * Math.log(MODERN_ACTIVITY / +sampleActivity)) / 0.693
+    );
+    if (rez == Infinity) return false;
+    if (rez == -Infinity) return false;
+    if (isNaN(rez)) return false;
+    if (rez < 0) return false;
+    return rez;
+  }
 }
 
 module.exports = {
-  dateSample
+  dateSample,
 };
